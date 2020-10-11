9aea10f88d385081bbbaa411d358a06762e1bd0e
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-10-11 13:36:24 +0530

    Night Vision Camera Setup Done

---
+++ Assets/Scripts/LightSettingsPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class LightSettingsPlayer : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume myVolume;
+    [SerializeField] PostProcessProfile standard;
+    [SerializeField] PostProcessProfile nightVision;
+    [SerializeField] GameObject nightVisionOverlay;
+
+    private bool isNightVision = false;
+
+    void Start()
+    {
+        nightVisionOverlay.gameObject.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            if(isNightVision == false)
+            {
+                myVolume.profile = nightVision;
+                nightVisionOverlay.gameObject.SetActive(true);
+                isNightVision = true; 
+            } else
+            {
+                myVolume.profile = standard;
+                isNightVision = false;
+                nightVisionOverlay.gameObject.SetActive(false);
+            }
+
+        }
+    }
+}
